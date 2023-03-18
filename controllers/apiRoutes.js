51547329a9c03319768b6c4c0667cc5e6cdd32e8
moviefan322 <philman202@yahoo.com>
2023-03-18 19:07:18 -0400

    added user:id and post and delete routes for user api

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -3,9 +3,47 @@ const User = require("../models/User");
 
 router.get("/users", async (req, res) => {
   try {
-    const userData = await User.findAll();
-    console.log(userData);
-    res.json(userData);
+    const UserData = await User.findAll();
+    console.log(UserData);
+    res.status(200).json(UserData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/users/:id", async (req, res) => {
+  try {
+    const UserData = await User.findByPk(req.params.id);
+    if (!UserData) {
+      return res.status(404).json({ message: "Could not find user!" });
+    }
+    console.log(UserData);
+    res.status(200).json(UserData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/users", async (req, res) => {
+  try {
+    const UserData = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.status(200).json({ message: "User created!" });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const UserData = User.destroy({ where: { id: req.params.id } });
+    if (!UserData) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User Deleted!" });
   } catch (err) {
     res.status(500).json(err);
   }
