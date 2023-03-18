1e2d049ccf0effca3a9672afb1f3cb23bcdd1658
moviefan322 <philman202@yahoo.com>
2023-03-18 18:46:28 -0400

    adds get route for /api/users

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -1,4 +1,14 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
+router.get("/users", async (req, res) => {
+  try {
+    const userData = await User.findAll();
+    console.log(userData);
+    res.json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-modules.export = router;
+module.exports = router;
