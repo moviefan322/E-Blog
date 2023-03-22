b5d8fe2718319c5e4bb0273db2d6ff269337b01f
moviefan322 <philman202@yahoo.com>
2023-03-21 23:56:20 -0400

    resolves edit post/save changes bug

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,70 +1,69 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const deleteButton = document.getElementById("deletepost");
+  const deleteButtons = document.querySelectorAll(".deletepost");
 
-  // Add click event listener to each delete button
-  deleteButton.addEventListener("click", () => {
-    // Get the post ID
-    const postId = document.getElementById("blog_id").textContent;
-    // Send a DELETE request to the server to delete the post
-    fetch(`/api/blogs/${postId}`, {
-      method: "DELETE",
-    })
-      .then((response) => {
-        // Reload the page to update the displayed posts
-        window.location.reload();
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  });
-
-  const editButton = document.getElementById("editpost");
-
-  editButton.addEventListener("click", () => {
-    const parentSection = editButton.parentNode;
-    const subjectEl = parentSection.querySelector("#subjectline");
-    const postlineEl = parentSection.querySelector("#postline");
-    const currentSubject = subjectEl.textContent.substring(9);
-    const currentPostline = postlineEl.textContent;
-
-    const subjectInput = document.createElement("input");
-    subjectInput.type = "text";
-    subjectInput.value = currentSubject;
-    subjectEl.replaceWith(subjectInput);
-
-    const postlineInput = document.createElement("textarea");
-    postlineInput.textContent = currentPostline;
-    postlineEl.replaceWith(postlineInput);
-
-    const saveButton = document.createElement("button");
-    saveButton.textContent = "Save Changes";
-    saveButton.classList.add("button");
-    parentSection.appendChild(saveButton);
-
-    const deleteButton = parentSection.querySelector("#deletepost");
-    deleteButton.remove();
-
-    editButton.remove();
-
-    const blogId = parentSection.querySelector("#blog_id").textContent;
-
-    saveButton.addEventListener("click", () => {
-      const newSubject = subjectInput.value;
-      const newPostline = postlineInput.value;
-
-      fetch(`/api/blogs/${blogId}`, {
-        method: "PUT",
-        body: JSON.stringify({ subject: newSubject, post: newPostline }),
-        headers: {
-          "Content-Type": "application/json",
-        },
+  deleteButtons.forEach((deleteButton) => {
+    deleteButton.addEventListener("click", () => {
+      // Get the post ID
+      const postId = document.getElementById("blog_id").textContent;
+      // Send a DELETE request to the server to delete the post
+      fetch(`/api/blogs/${postId}`, {
+        method: "DELETE",
       })
         .then((response) => {
+          // Reload the page to update the displayed posts
           window.location.reload();
         })
         .catch((error) => {
           console.error(error);
         });
+    });
+  });
+
+  const editButtons = document.querySelectorAll(".editpost");
+
+  editButtons.forEach((editButton) => {
+    editButton.addEventListener("click", () => {
+      editButton.style.display = "none";
+      const parentSection = editButton.parentNode;
+      const subjectEl = parentSection.querySelector("#subjectline");
+      const postlineEl = parentSection.querySelector("#postline");
+      const currentSubject = subjectEl.textContent.substring(9);
+      const currentPostline = postlineEl.textContent;
+
+      const subjectInput = document.createElement("input");
+      subjectInput.type = "text";
+      subjectInput.value = currentSubject;
+      subjectEl.replaceWith(subjectInput);
+
+      const postlineInput = document.createElement("textarea");
+      postlineInput.textContent = currentPostline;
+      postlineEl.replaceWith(postlineInput);
+
+      const saveButton = document.createElement("button");
+      saveButton.textContent = "Save Changes";
+      saveButton.classList.add("button");
+      parentSection.appendChild(saveButton);
+
+      const blogId = parentSection.querySelector("#blog_id").textContent;
+
+      saveButton.addEventListener("click", () => {
+        const newSubject = subjectInput.value;
+        const newPostline = postlineInput.value;
+
+        fetch(`/api/blogs/${blogId}`, {
+          method: "PUT",
+          body: JSON.stringify({ subject: newSubject, post: newPostline }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => {
+            window.location.reload();
+          })
+          .catch((error) => {
+            console.error(error);
+          });
+      });
     });
   });
 });
