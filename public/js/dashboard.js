4f50ec1a9fd27ada8976721d7f7bb2e500d3cc6f
moviefan322 <philman202@yahoo.com>
2023-03-21 21:00:30 -0400

    edit button now working

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -17,4 +17,54 @@ document.addEventListener("DOMContentLoaded", () => {
         console.error(error);
       });
   });
+
+  const editButton = document.getElementById("editpost");
+
+  editButton.addEventListener("click", () => {
+    const parentSection = editButton.parentNode;
+    const subjectEl = parentSection.querySelector("#subjectline");
+    const postlineEl = parentSection.querySelector("#postline");
+    const currentSubject = subjectEl.textContent.substring(9);
+    const currentPostline = postlineEl.textContent;
+
+    const subjectInput = document.createElement("input");
+    subjectInput.type = "text";
+    subjectInput.value = currentSubject;
+    subjectEl.replaceWith(subjectInput);
+
+    const postlineInput = document.createElement("textarea");
+    postlineInput.textContent = currentPostline;
+    postlineEl.replaceWith(postlineInput);
+
+    const saveButton = document.createElement("button");
+    saveButton.textContent = "Save Changes";
+    saveButton.classList.add("button");
+    parentSection.appendChild(saveButton);
+
+    const deleteButton = parentSection.querySelector("#deletepost");
+    deleteButton.remove();
+
+    editButton.remove();
+
+    const blogId = parentSection.querySelector("#blog_id").textContent;
+
+    saveButton.addEventListener("click", () => {
+      const newSubject = subjectInput.value;
+      const newPostline = postlineInput.value;
+
+      fetch(`/api/blogs/${blogId}`, {
+        method: "PUT",
+        body: JSON.stringify({ subject: newSubject, post: newPostline }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => {
+          window.location.reload();
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    });
+  });
 });
