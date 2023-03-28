ec9ac16e3be2b819c27d1c8ce7bc230fdcf83ec2
moviefan322 <philman202@yahoo.com>
2023-03-27 20:23:19 -0400

    troubleshoots .env config

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,26 @@
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const Sequelize = require("sequelize");
-require("dotenv").config();
+
+console.log(process.env.DB_NAME);
+console.log(process.env.DB_USER);
+console.log(process.env.DB_PASSWORD);
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
-  sequelize = new Sequelize("eblog_db", "root", "tesseradecades", {
-    host: "localhost",
-    dialect: "mysql",
-    port: 3306,
-  });
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
 }
 
 module.exports = sequelize;
